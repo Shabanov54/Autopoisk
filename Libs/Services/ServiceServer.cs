44f1b7022e862a81e34db39e96101594bb926132
Станислав Шабанов <marazm56@gmail.com>
2021-10-05 19:04:38 +0700

    Добавил NuGet пакет ServiceModel

--- Libs/Services/ServiceServer.cs
+++ Libs/Services/ServiceServer.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
 
 namespace Services
 {
     public class ServiceServer
     {
+        public void CreateServiceServer()
+        {
+            var serviceAddress = "127.0.0.1:10000";
+            var serviceName = "MyService";
+
+        }
     }
 }
