68b136145e6c02c83f10c24d746176320bb78453
Станислав Шабанов <marazm56@gmail.com>
2021-10-05 21:42:17 +0700

    Парвка

---
+++ Catalogs/WCFService/App_Code/ServiceLibs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    public class ServiceLibs : ServiceILibs
+    {
+        public string TimerDownloads(string timer)
+        {
+            string outputMessage;
+
+            return outputMessage = "Время потраченное на загрузку страницы : " + timer; ;
+        }
+    }
