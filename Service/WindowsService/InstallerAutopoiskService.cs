bde8975c7e5c45680af1288a7d9dedfe3d809e93
Станислав Шабанов <marazm56@gmail.com>
2021-10-11 11:53:19 +0700

    Прописал установщик службы

--- Service/WindowsService/InstallerAutopoiskService.cs
+++ Service/WindowsService/InstallerAutopoiskService.cs
@@ -4,16 +4,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.ServiceProcess;
 using System.Threading.Tasks;
 
 namespace WindowsService
 {
     [RunInstaller(true)]
-    public partial class InstallerAutopoiskService : System.Configuration.Install.Installer
+    public partial class InstallerAutopoiskService : Installer
+
     {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
         public InstallerAutopoiskService()
         {
             InitializeComponent();
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalService;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "AutopoiskService";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
         }
     }
 }
