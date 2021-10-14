dfed29ee720c42b0edda6672b79910c4d9c36cd6
Станислав Шабанов <marazm56@gmail.com>
2021-10-14 13:30:30 +0700

    Создал отдельный класс в службе, которая при старте будет работать в отдельном потоке

--- Service/PostgresCreateConnService/CreateConnect.cs
+++ Service/PostgresCreateConnService/CreateConnect.cs
@@ -6,12 +6,18 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PostgresCreateConnService
 {
     public partial class CreateConnect : ServiceBase
     {
+        private static string Host = "mydemoserver.postgres.database.azure.com";
+        private static string User = "mylogin@mydemoserver";
+        private static string DBname = "mypgsqldb";
+        private static string Password = "<server_admin_password>";
+        private static string Port = "5432";
         public CreateConnect()
         {
 
@@ -20,10 +26,20 @@ namespace PostgresCreateConnService
 
         protected override void OnStart(string[] args)
         {
+            RunService service = new RunService();
+            Thread connectToDB = new Thread(new ThreadStart(service.Start));
+            service.Start();
         }
 
         protected override void OnStop()
         {
+        }
+        public class RunService
+        {
+            public void Start()
+            {
+
+            }
         }
     }
 }
