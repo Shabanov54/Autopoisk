702ad28edfca6843294c855b1f6d963b4321f7ba
Станислав Шабанов <marazm56@gmail.com>
2021-10-15 13:34:50 +0700

    Создал новый проект на основе консольного приложения(Core)

---
+++ Catalogs/AutopoiskEFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutopoiskEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
